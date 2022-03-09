445c04ffb131622eff5478a67fa659c311939711
Frankie <frankieislost@protonmail.com>
2022-03-09 15:30:14 -0800

    pages pricing

--- src/Pages.sol
+++ src/Pages.sol
@@ -4,11 +4,12 @@ pragma solidity >=0.8.0;
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {Strings} from "openzeppelin/utils/Strings.sol";
 import {PRBMathSD59x18} from "prb-math/PRBMathSD59x18.sol";
+import {VRGDA} from "./VRGDA.sol";
 
 import {Goop} from "./Goop.sol";
 
 ///@notice Pages is an ERC721 that can hold art drawn
-contract Pages is ERC721("Pages", "PAGE") {
+contract Pages is ERC721("Pages", "PAGE"), VRGDA {
     using Strings for uint256;
     using PRBMathSD59x18 for int256;
 
@@ -18,6 +19,9 @@ contract Pages is ERC721("Pages", "PAGE") {
 
     ///@notice id of last mint
     uint256 internal currentId;
+
+    ///@notice
+    uint256 internal numMintedFromGoop;
 
     ///@notice base token URI
     string internal constant BASE_URI = "";
@@ -31,21 +35,31 @@ contract Pages is ERC721("Pages", "PAGE") {
     /// ---- Pricing Parameters ----
     /// ----------------------------
 
-    int256 private immutable priceScale = 1;
+    int256 private immutable logisticScale = PRBMathSD59x18.fromInt(10024);
 
-    int256 private immutable timeScale = 1;
+    int256 private immutable timeScale =
+        PRBMathSD59x18.fromInt(1).div(PRBMathSD59x18.fromInt(30));
 
-    int256 private immutable timeShift = 1;
+    int256 private immutable timeShift = PRBMathSD59x18.fromInt(180);
 
-    int256 private immutable initialPrice = 1;
+    int256 private immutable initialPrice = PRBMathSD59x18.fromInt(420);
 
-    int256 private immutable periodPriceDecrease = 1;
+    int256 private immutable periodPriceDecrease =
+        PRBMathSD59x18.fromInt(1).div(PRBMathSD59x18.fromInt(4));
 
-    int256 private immutable perPeriodPostSwitchover = 1;
+    int256 private immutable perPeriodPostSwitchover =
+        PRBMathSD59x18.fromInt(10).div(PRBMathSD59x18.fromInt(3));
 
-    int256 private immutable switchoverTime = 1;
+    int256 private immutable switchoverTime = PRBMathSD59x18.fromInt(360);
 
-    uint256 private lastPurchaseTime;
+    int256 private immutable priceScaling =
+        PRBMathSD59x18.fromInt(3).div(PRBMathSD59x18.fromInt(4));
+
+    ///@notice number of pages sold before we switch pricing function
+    uint256 private numPagesSwitch = 9975;
+
+    ///@notice start of public mint
+    uint256 private mintStart;
 
     /// -----------------------
     /// ------ Authority ------
@@ -61,7 +75,17 @@ contract Pages is ERC721("Pages", "PAGE") {
 
     error Unauthorized();
 
-    constructor(address _goop, address _drawAddress) {
+    error MintNotStarted();
+
+    constructor(address _goop, address _drawAddress)
+        VRGDA(
+            logisticScale,
+            timeScale,
+            timeShift,
+            initialPrice,
+            periodPriceDecrease
+        )
+    {
         goop = Goop(_goop);
         drawAddress = _drawAddress;
         //deployer has mint authority
@@ -81,68 +105,57 @@ contract Pages is ERC721("Pages", "PAGE") {
         isDrawn[tokenId] = true;
     }
 
-    ///@notice burn goop and mint page
+    ///@notice mint a page by burning goop
     function mint() public {
-        uint256 price = mintCost();
+        //mint start has not been set, or mint has not started
+        if (mintStart == 0 || block.timestamp < mintStart) {
+            revert MintNotStarted();
+        }
+        uint256 price = pagePrice();
         if (goop.balanceOf(msg.sender) < price) {
             revert InsufficientBalance();
         }
         goop.burn(msg.sender, price);
         _mint(msg.sender, ++currentId);
+        numMintedFromGoop++;
     }
 
+    ///@notice set mint start timestamp for regular minting
+    function setMintStart(uint256 _mintStart) public only(mintAddress) {
+        mintStart = _mintStart;
+    }
+
+    ///@notice mint by authority without paying mint cost
     function mintByAuth(address addr) public only(mintAddress) {
         _mint(addr, ++currentId);
     }
 
-    function mintCost() public view returns (uint256) {
-        uint256 threshold = switchThreshold();
-        if (threshold < currentId) {
-            return preSwitchPrice();
-        } else {
-            return postSwitchPrice(threshold);
-        }
+    ///@notice calculate the mint cost of a page. If number of sales
+    ///is below a pre-defined threshold, we use VRGDA pricing algorithm
+    ///otherwise, we use the post-switch pricing formula
+    function pagePrice() public view returns (uint256) {
+        uint256 timeSinceStart = block.timestamp - mintStart;
+
+        return
+            (currentId < numPagesSwitch)
+                ? getPrice(timeSinceStart, numMintedFromGoop)
+                : postSwitchPrice(timeSinceStart);
     }
 
-    function switchThreshold() internal view returns (uint256) {
-        int256 t = int256(block.timestamp - lastPurchaseTime);
-        int256 exp = PRBMathSD59x18
-            .fromInt(-1)
-            .mul(timeScale)
-            .mul(t - timeShift)
-            .exp();
-        int256 res = priceScale.div(PRBMathSD59x18.fromInt(1) + exp);
-        return uint256(res.toInt());
-    }
-
-    function preSwitchPrice() internal view returns (uint256) {
-        int256 exp = PRBMathSD59x18.fromInt(
-            int256(block.timestamp - lastPurchaseTime)
-        ) -
-            timeShift +
-            (
-                (
-                    (PRBMathSD59x18.fromInt(-1) + priceScale).div(
-                        PRBMathSD59x18.fromInt(int256(currentId))
-                    )
-                ).ln().div(timeScale)
-            );
-        int256 scalingFactor = (PRBMathSD59x18.fromInt(1) - periodPriceDecrease)
-            .pow(exp);
-        int256 price = initialPrice.mul(scalingFactor);
-        return uint256(price.toInt());
-    }
-
-    function postSwitchPrice(uint256 threshold)
+    ///@notice calculate the mint cost of a page after switch
+    function postSwitchPrice(uint256 timeSinceStart)
         internal
         view
         returns (uint256)
     {
-        int256 t = int256(block.timestamp - lastPurchaseTime).fromInt();
-        int256 delta = int256(currentId - threshold).fromInt();
-        int256 fInv = delta.div(perPeriodPostSwitchover) + switchoverTime;
-        int256 log = -((PRBMathSD59x18.fromInt(1) - periodPriceDecrease).ln());
-        int256 scalingFactor = log.mul(fInv - t).exp();
+        int256 fInv = (PRBMathSD59x18.fromInt(int256(numMintedFromGoop + 1)) -
+            PRBMathSD59x18.fromInt(int256(numPagesSwitch))).div(
+                perPeriodPostSwitchover
+            ) + switchoverTime;
+        int256 time = PRBMathSD59x18.fromInt(int256(timeSinceStart)).div(
+            dayScaling
+        );
+        int256 scalingFactor = priceScaling.pow(time - fInv);
         int256 price = initialPrice.mul(scalingFactor);
         return uint256(price.toInt());
     }
