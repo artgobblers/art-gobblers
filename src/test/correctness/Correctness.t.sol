45ab877af86f6f1e312c0e2ea7938a4e995008f0
Frankie <frankieislost@protonmail.com>
2022-03-10 19:36:59 -0800

    tempo

---
+++ src/test/correctness/Correctness.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {console} from "../utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+contract CorrectnessTest is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    function testBasic() public {
+        string[] memory inputs = new string[](2);
+        inputs[0] = "python3";
+        inputs[1] = "src/test/correctness/compute.py";
+        bytes memory res = vm.ffi(inputs);
+        console.log("pybytes");
+        console.logBytes(res);
+        // uint256 num = abi.decode(res, (uint256));
+        bytes memory enc = abi.encode(4000);
+        console.log("solbytes");
+        console.logBytes(enc);
+
+        // console.log(num);
+        // console.logBytes(res);
+        // Data memory data = abi.decode(res, (Data));
+        // assertEq(data.name, name);
+        // for (uint256 i = 0; i < attributes.length; i++) {
+        //     assertEq(data.attributes[i], attributes[i]);
+        // }
+    }
+}
---
+++ src/test/correctness/compute.py
@@ -0,0 +1,14 @@
+import math
+from eth_abi import encode_single, decode_single, encode_abi
+
+def gobbler_price(t, sold, p_0, per_period_price_decrease, scale, time_scale, time_shift):
+    initial_value = scale / (1 + math.exp(time_scale * time_shift))
+    logistic_value = sold + initial_value
+    return (1 - per_period_price_decrease) ** (t - time_shift + (math.log(-1 + scale / logistic_value) / time_scale)) * p_0
+
+# enc = encode_single('(uint256, uint256)', [40000, 500000])
+enc = encode_single('uint256', 4000)
+# print(enc.hex())
+bb = b'\x01\x02\x03'
+print(bb.hex())
+# print(decode_single('uint8', enc))
