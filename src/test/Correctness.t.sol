ac995289b284b5c2135d66f90a4c213a4000675a
t11s <greenfeatherhelp@gmail.com>
2022-04-17 21:51:15 -0700

    ✅ Correctness tests

--- src/test/Correctness.t.sol
+++ src/test/Correctness.t.sol
@@ -1,86 +1,67 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
-import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {MockLogisticVRGDA} from "./utils/mocks/MockLogisticVRGDA.sol";
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 import {Strings} from "openzeppelin/utils/Strings.sol";
-import {PRBMathSD59x18} from "prb-math/PRBMathSD59x18.sol";
-import {console} from "./utils/Console.sol";
 
-// todo: get rid of PRB
 contract CorrectnessTest is DSTestPlus {
     using Strings for uint256;
-    using PRBMathSD59x18 for int256;
+
+    uint256 internal immutable FIVE_YEARS = 365 days * 5;
+
+    uint256 internal immutable MAX_GOOP_MINT = 7990;
+
+    int256 internal immutable LOGISTIC_SCALE = 7990 * 2e18;
+
+    int256 internal immutable INITIAL_PRICE = 6.9e18;
+
+    int256 internal immutable PER_PERIOD_PRICE_DECREASE = 0.31e18;
+
+    int256 internal immutable TIME_SCALE = 0.014e18;
 
     Vm internal immutable vm = Vm(HEVM_ADDRESS);
 
-    uint256 internal constant MAX_GOOP_MINT = 7990;
+    MockLogisticVRGDA internal vrgda;
 
-    int256 internal immutable initialPrice = PRBMathSD59x18.fromInt(69);
+    function setUp() public {
+        vrgda = new MockLogisticVRGDA(INITIAL_PRICE, PER_PERIOD_PRICE_DECREASE, LOGISTIC_SCALE, TIME_SCALE);
+    }
 
-    int256 internal immutable logisticScale = PRBMathSD59x18.fromInt(int256((MAX_GOOP_MINT + 1) * 2));
+    function testFFICorrectness(uint256 timeSinceStart, uint256 numSold) public {
+        // Limit num sold to max mint.
+        numSold = bound(numSold, 0, MAX_GOOP_MINT);
 
-    int256 internal immutable timeScale = PRBMathSD59x18.fromInt(1).div(PRBMathSD59x18.fromInt(60));
+        // Limit mint time to 5 years.
+        timeSinceStart = bound(timeSinceStart, 0, FIVE_YEARS);
 
-    int256 internal immutable periodPriceDecrease = PRBMathSD59x18.fromInt(1).div(PRBMathSD59x18.fromInt(4));
+        // Calculate actual price from VRGDA.
+        try vrgda.getPrice(timeSinceStart, numSold) returns (uint256 actualPrice) {
+            // Calculate expected price from python script.
+            uint256 expectedPrice = calculatePrice(
+                timeSinceStart,
+                numSold,
+                INITIAL_PRICE,
+                PER_PERIOD_PRICE_DECREASE,
+                LOGISTIC_SCALE,
+                TIME_SCALE
+            );
 
-    uint256 internal immutable FIVE_YEARS = 52 weeks * 5;
+            if (expectedPrice < 10) return; // For really small prices we can't expect them to be equal.
 
-    //fuzz test correctness of pricing function for different combinations of time and quantity sold.
-    //we match all other parameters (initialPrice, timescale, etc...) to the ones used for
-    //gobbler pricing specifically.
-    // function testCorrectness(uint256 timeSinceStart, uint256 numSold) public {
-    //     //limit num sold to max mint
-    //     numSold = bound(numSold, 0, MAX_GOOP_MINT);
-    //     //limit mint time to 5 yeras
-    //     timeSinceStart = bound(timeSinceStart, 0, FIVE_YEARS);
-
-    //     checkPriceWithParameters(
-    //         timeSinceStart,
-    //         numSold,
-    //         initialPrice,
-    //         periodPriceDecrease,
-    //         logisticScale,
-    //         timeScale
-    //     );
-    // }
-
-    function checkPriceWithParameters(
-        uint256 _timeSinceStart,
-        uint256 _numSold,
-        int256 _initialPrice,
-        int256 perPeriodPriceDecrease,
-        int256 _logisticScale,
-        int256 _timeScale
-    ) private {
-        MockLogisticVRGDA vrgda = new MockLogisticVRGDA(
-            _initialPrice,
-            perPeriodPriceDecrease,
-            _logisticScale,
-            _timeScale
-        );
-        //calculate actual price from gda
-        uint256 actualPrice = vrgda.getPrice(_timeSinceStart, _numSold);
-        //calculate expected price from python script
-        uint256 expectedPrice = calculatePrice(
-            _timeSinceStart,
-            _numSold,
-            _initialPrice,
-            perPeriodPriceDecrease,
-            _logisticScale,
-            _timeScale
-        );
-        if (actualPrice == expectedPrice) return;
-        //equal within 1 percent
-        assertRelApproxEq(actualPrice, expectedPrice, 1e16);
+            // Equal within 1 percent.
+            assertRelApproxEq(actualPrice, expectedPrice, 0.01e18);
+        } catch {
+            // If it reverts that's fine, there are some bounds on the function, they are tested in VRGDAs.t.sol
+        }
     }
 
     function calculatePrice(
         uint256 _timeSinceStart,
         uint256 _numSold,
         int256 _initialPrice,
-        int256 perPeriodPriceDecrease,
+        int256 _perPeriodPriceDecrease,
         int256 _logisticScale,
         int256 _timeScale
     ) private returns (uint256) {
@@ -95,15 +76,14 @@ contract CorrectnessTest is DSTestPlus {
         inputs[7] = "--initial_price";
         inputs[8] = uint256(_initialPrice).toString();
         inputs[9] = "--per_period_price_decrease";
-        inputs[10] = uint256(perPeriodPriceDecrease).toString();
+        inputs[10] = uint256(_perPeriodPriceDecrease).toString();
         inputs[11] = "--logistic_scale";
         inputs[12] = uint256(_logisticScale).toString();
         inputs[13] = "--time_scale";
         inputs[14] = uint256(_timeScale).toString();
         inputs[15] = "--time_shift";
         inputs[16] = uint256(0).toString();
-        bytes memory res = vm.ffi(inputs);
-        uint256 price = abi.decode(res, (uint256));
-        return price;
+
+        return abi.decode(vm.ffi(inputs), (uint256));
     }
 }
