67e0015c4b9741bd94e5e1dbe10b41841ee48fbf
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2022-04-22 15:19:48 -0700

    change constant

--- src/ArtGobblers.sol
+++ src/ArtGobblers.sol
@@ -49,6 +49,10 @@ contract ArtGobblers is GobblersERC1155B, LogisticVRGDA, VRFConsumerBase, ERC115
     /// @notice Maximum amount of mintable legendary gobblers.
     uint256 private constant LEGENDARY_SUPPLY = 10;
 
+    /// @notice Maximum mintable from goop. This number 90% of the supply excluding whitelist and legendaries.
+    /// The remaining 10% is minted directly to the lockup vault
+    uint256 private constant MINTABLE_FROM_GOOP = ((MAX_SUPPLY - WHITELIST_SUPPLY - LEGENDARY_SUPPLY) * 90) / 100;
+
     /*//////////////////////////////////////////////////////////////
                             URI CONFIGURATION
     //////////////////////////////////////////////////////////////*/
@@ -204,7 +208,7 @@ contract ArtGobblers is GobblersERC1155B, LogisticVRGDA, VRFConsumerBase, ERC115
             // Logistic scale. We multiply by 2x (as a wad)
             // to account for the subtracted initial value,
             // and add 1 to ensure all the tokens can be sold:
-            int256(MAX_SUPPLY - WHITELIST_SUPPLY - LEGENDARY_SUPPLY + 1) * 2e18,
+            int256(MINTABLE_FROM_GOOP + 1) * 2e18,
             0.014e18 // Time scale.
         )
     {
