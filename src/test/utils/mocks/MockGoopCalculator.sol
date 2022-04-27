28644526b8c81315ab311d59971cfdd467f1cf20
t11s <greenfeatherhelp@gmail.com>
2022-04-27 08:21:58 -0700

    Update MockGoopCalculator.sol

--- src/test/utils/mocks/MockGoopCalculator.sol
+++ src/test/utils/mocks/MockGoopCalculator.sol
@@ -5,6 +5,8 @@ import {FixedPointMathLib as Math} from "solmate/utils/FixedPointMathLib.sol";
 
 contract MockGoopCalculator {
     /// @notice Compute goop balance based on emission multiple, last balance, and days
+    /// @dev Forked from https://github.com/FrankieIsLost/art-gobblers/blob/master/src/ArtGobblers.sol#L579-L595
+    /// @dev Must be kept up to date with the goopBalance function's corresponding calculations in ArtGobblers.sol.
     function computeGoopBalance(
         uint256 emissionMultiple,
         uint256 lastBalanceWad,
