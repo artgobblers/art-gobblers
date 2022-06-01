f5fb8a42c3604909d34e5c25255b0b4546bfac49
t11s <greenfeatherhelp@gmail.com>
2022-05-31 23:27:59 -0700

    ✅ OptimizationsTest

---
+++ src/test/Optimizations.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+contract OptimizationsTest is DSTestPlus {
+    function testFuzzCurrentIdMultipleBranchlessOptimization(uint256 swapIndex) public {
+        /*//////////////////////////////////////////////////////////////
+                                  BRANCHLESS
+        //////////////////////////////////////////////////////////////*/
+
+        uint256 newCurrentIdMultipleBranchless = 9; // For beyond 7963.
+        assembly {
+            // prettier-ignore
+            newCurrentIdMultipleBranchless := sub(sub(sub(newCurrentIdMultipleBranchless, 
+                lt(swapIndex, 7964)), lt(swapIndex, 5673)), lt(swapIndex, 3055)
+            )
+        }
+
+        /*//////////////////////////////////////////////////////////////
+                                  BRANCHED
+        //////////////////////////////////////////////////////////////*/
+
+        uint256 newCurrentIdMultipleBranched = 9; // For beyond 7963.
+        if (swapIndex <= 3054) newCurrentIdMultipleBranched = 6;
+        else if (swapIndex <= 5672) newCurrentIdMultipleBranched = 7;
+        else if (swapIndex <= 7963) newCurrentIdMultipleBranched = 8;
+
+        /*//////////////////////////////////////////////////////////////
+                                EQUIVALENCE
+        //////////////////////////////////////////////////////////////*/
+
+        assertEq(newCurrentIdMultipleBranchless, newCurrentIdMultipleBranched);
+    }
+}
