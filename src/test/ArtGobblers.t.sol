d3a0b14969cfe39da20266eec18e36f14ec9c47e
frankie <88221686+FrankieIsLost@users.noreply.github.com>
2022-06-17 14:49:52 -0400

    test

--- src/test/ArtGobblers.t.sol
+++ src/test/ArtGobblers.t.sol
@@ -165,7 +165,7 @@ contract ArtGobblersTest is DSTestPlus, ERC1155TokenReceiver {
     function testPricingBasic() public {
         // VRGDA targets this number of mints at given time.
         uint256 timeDelta = 120 days;
-        uint256 numMint = 4384;
+        uint256 numMint = 2876;
 
         vm.warp(block.timestamp + timeDelta);
 
