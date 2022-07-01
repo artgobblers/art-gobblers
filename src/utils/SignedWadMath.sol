4b77f99d53c3497bef33e4ec58d8585afc39dab2
t11s <greenfeatherhelp@gmail.com>
2022-07-01 13:26:28 -0700

    ♻️ Cleanup remco funcs

--- src/utils/SignedWadMath.sol
+++ src/utils/SignedWadMath.sol
@@ -65,15 +65,14 @@ function unsafeDiv(int256 x, int256 y) pure returns (int256 z) {
     }
 }
 
-// TODO: update this, just a draft of remco exp
 function wadExp(int256 x) pure returns (int256 r) {
     unchecked {
         // When the result is < 0.5 we return zero. This happens when
         // x <= floor(log(0.5e18) * 1e18) ~ -42e18
         if (x <= -42139678854452767551) return 0;
 
-        // When the result is > (2**255 - 1) / 1e18 we can not represent it
-        // as an int256. This happens when x >= floor(log((2**255 -1) / 1e18) * 1e18) ~ 135.
+        // When the result is > (2**255 - 1) / 1e18 we can not represent it as an
+        // int. This happens when x >= floor(log((2**255 - 1) / 1e18) * 1e18) ~ 135.
         if (x >= 135305999368893231589) revert("EXP_OVERFLOW");
 
         // x is now in the range (-42, 136) * 1e18. Convert to (-42, 136) * 2**96
@@ -81,8 +80,8 @@ function wadExp(int256 x) pure returns (int256 r) {
         // is a multiplication by 1e18 / 2**96 = 5**18 / 2**78.
         x = (x << 78) / 5**18;
 
-        // Reduce range of x to (-½ ln 2, ½ ln 2) * 2**96 by factoring out powers of two
-        // such that exp(x) = exp(x') * 2**k, where k is an integer.
+        // Reduce range of x to (-½ ln 2, ½ ln 2) * 2**96 by factoring out powers
+        // of two such that exp(x) = exp(x') * 2**k, where k is an integer.
         // Solving this gives k = round(x / log(2)) and x' = x - k * log(2).
         int256 k = ((x << 96) / 54916777467707473351141471128 + 2**95) >> 96;
         x = x - k * 54916777467707473351141471128;
@@ -95,7 +94,7 @@ function wadExp(int256 x) pure returns (int256 r) {
         y = ((y * x) >> 96) + 57155421227552351082224309758442;
         int256 p = y + x - 94201549194550492254356042504812;
         p = ((p * y) >> 96) + 28719021644029726153956944680412240;
-        p = p * x + (4385272521454847904632057985693276 << 96);
+        p = p * x + (4385272521454847904659076985693276 << 96);
 
         // We leave p in 2**192 basis so we don't need to scale it back up for the division.
         int256 q = x - 2855989394907223263936484059900;
@@ -106,7 +105,7 @@ function wadExp(int256 x) pure returns (int256 r) {
         q = ((q * x) >> 96) + 26449188498355588339934803723976023;
 
         assembly {
-            // Div in assembly because solidity adds a zero check despite the `unchecked`.
+            // Div in assembly because solidity adds a zero check despite the unchecked.
             // The q polynomial won't have zeros in the domain as all its roots are complex.
             // No scaling is necessary because p is already 2**96 too large.
             r := sdiv(p, q)
@@ -115,25 +114,22 @@ function wadExp(int256 x) pure returns (int256 r) {
         // r should be in the range (0.09, 0.25) * 2**96.
 
         // We now need to multiply r by:
-        // - the scale factor s = ~6.031367120.
-        // - the 2**k factor from the range reduction.
-        // - the 1e18 / 2**96 factor for base conversion.
+        // * the scale factor s = ~6.031367120.
+        // * the 2**k factor from the range reduction.
+        // * the 1e18 / 2**96 factor for base conversion.
         // We do this all at once, with an intermediate result in 2**213
         // basis, so the final right shift is always by a positive amount.
         r = int256((uint256(r) * 3822833074963236453042738258902158003155416615667) >> uint256(195 - k));
     }
 }
 
-// Computes ln(x) in 1e18 fixed point.
-// Reverts if x is negative or zero.
-// Consumes 670 gas.
 function wadLn(int256 x) pure returns (int256 r) {
     unchecked {
         require(x > 0, "UNDEFINED");
 
         // We want to convert x from 10**18 fixed point to 2**96 fixed point.
-        // We do this by multiplying by 2**96 / 10**18.
-        // But since ln(x * C) = ln(x) + ln(C), we can simply do nothing here
+        // We do this by multiplying by 2**96 / 10**18. But since
+        // ln(x * C) = ln(x) + ln(C), we can simply do nothing here
         // and add ln(2**96 / 10**18) at the end.
 
         assembly {
@@ -153,8 +149,8 @@ function wadLn(int256 x) pure returns (int256 r) {
         x <<= uint256(159 - k);
         x = int256(uint256(x) >> 159);
 
-        // Evaluate using a (8, 8)-term rational approximation
-        // p is made monic, we will multiply by a scale factor later
+        // Evaluate using a (8, 8)-term rational approximation.
+        // p is made monic, we will multiply by a scale factor later.
         int256 p = x + 3273285459638523848632254066296;
         p = ((p * x) >> 96) + 24828157081833163892658089445524;
         p = ((p * x) >> 96) + 43456485725739037958740375743393;
@@ -164,7 +160,7 @@ function wadLn(int256 x) pure returns (int256 r) {
         p = p * x - (795164235651350426258249787498 << 96);
 
         // We leave p in 2**192 basis so we don't need to scale it back up for the division.
-        // q is monic by convention
+        // q is monic by convention.
         int256 q = x + 5573035233440673466300451813936;
         q = ((q * x) >> 96) + 71694874799317883764090561454958;
         q = ((q * x) >> 96) + 283447036172924575727196451306956;
@@ -172,21 +168,21 @@ function wadLn(int256 x) pure returns (int256 r) {
         q = ((q * x) >> 96) + 204048457590392012362485061816622;
         q = ((q * x) >> 96) + 31853899698501571402653359427138;
         q = ((q * x) >> 96) + 909429971244387300277376558375;
-
         assembly {
-            // Div in assembly because solidity adds a zero check despite the `unchecked`.
-            // The q polynomial is known not to have zeros in the domain. (All roots are complex)
+            // Div in assembly because solidity adds a zero check despite the unchecked.
+            // The q polynomial is known not to have zeros in the domain.
             // No scaling required because p is already 2**96 too large.
             r := sdiv(p, q)
         }
 
         // r is in the range (0, 0.125) * 2**96
 
-        // Finalization, we need to
+        // Finalization, we need to:
         // * multiply by the scale factor s = 5.549…
         // * add ln(2**96 / 10**18)
         // * add k * ln(2)
         // * multiply by 10**18 / 2**96 = 5**18 >> 78
+
         // mul s * 5e18 * 2**96, base is now 5**18 * 2**192
         r *= 1677202110996718588342820967067443963516166;
         // add ln(2) * k * 5e18 * 2**192
