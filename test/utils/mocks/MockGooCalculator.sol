ea7fcf97a07129afc557ccf57ca328938bfc1218
t11s <greenfeatherhelp@gmail.com>
2022-07-03 22:15:42 -0700

    ♻️ Update MockGooCalculator

--- test/utils/mocks/MockGooCalculator.sol
+++ test/utils/mocks/MockGooCalculator.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import {FixedPointMathLib as Math} from "solmate/utils/FixedPointMathLib.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 contract MockGooCalculator {
-    /// @notice Compute goo balance based on emission multiple, last balance, and days
+    using FixedPointMathLib for uint256;
+
+    /// @notice Compute goo balance based on emission multiple, last balance, and days elapsed.
     /// @dev Must be kept up to date with the gooBalance function's corresponding emission balance calculations in ArtGobblers.sol.
-    /// @dev Forked from https://github.com/artgobblers/art-gobblers/blob/807809821c615ec46b7f42e838346c5c57a402f6/src/ArtGobblers.sol#L579-L595
+    /// @dev Forked from https://github.com/artgobblers/art-gobblers/blob/2f19bc901ed2f1bfedeb6f113b073bfc3585386a/src/ArtGobblers.sol#L693-L708
     function computeGooBalance(
         uint256 emissionMultiple,
         uint256 lastBalanceWad,
         uint256 daysElapsedWad
     ) public pure returns (uint256) {
         unchecked {
-            uint256 daysElapsedSquaredWad = Math.mulWadDown(daysElapsedWad, daysElapsedWad); // Need to use wad math here.
+            uint256 daysElapsedSquaredWad = daysElapsedWad.mulWadDown(daysElapsedWad); // Need to use wad math here.
 
             // prettier-ignore
             return lastBalanceWad + // The last recorded balance.
@@ -23,12 +25,11 @@ contract MockGooCalculator {
             // Shift right by 2 is equivalent to division by 4.
             ((emissionMultiple * daysElapsedSquaredWad) >> 2) +
 
-            Math.mulWadDown(
-                daysElapsedWad, // Must mulWad because both terms are wads.
+            daysElapsedWad.mulWadDown( // Terms are wads, so must mulWad.
                 // No wad multiplication for emissionMultiple * lastBalance
                 // because emissionMultiple is a plain integer with no decimals.
                 // We multiply the sqrt's radicand by 1e18 because it expects ints.
-                Math.sqrt(emissionMultiple * lastBalanceWad * 1e18)
+                (emissionMultiple * lastBalanceWad * 1e18).sqrt()
             );
         }
     }
