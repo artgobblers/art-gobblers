749d06f91917272eb066e753fdc26d3e295e4c79
t11s <greenfeatherhelp@gmail.com>
2022-09-07 00:28:30 -0700

    ♻️ Word align

--- src/utils/lib/LibString.sol
+++ src/utils/lib/LibString.sol
@@ -8,15 +8,15 @@ library LibString {
         if (n == 0) return "0"; // Otherwise it'd output an empty string for 0.
 
         assembly {
-            let k := 78 // Start with the max length a uint256 string could be.
+            let k := 96 // The max length of a uint256 string, 78 bytes, word aligned to 96 bytes as is convention.
 
-            // We'll store our string at the first chunk of free memory.
+            // Grab the destination of the free memory pointer. We'll store our string length and content offset from it.
             str := mload(0x40)
 
             // Update the free memory pointer to prevent overriding our string. We
             // need to allocate 5 32-byte words for our string, so we increment the free
             // memory pointer by 160 (32 * 5). The first word is used to store the length of the
-            // string. We need up to 3 words to store digits (78 bytes). We also leave an extra word
+            // string. We need up to 3 words to store digits (96 bytes). We also leave an extra word
             // of zeros at the end, such that there are no dirty bits at the end of our string encoding.
             mstore(0x40, add(str, 160))
 
@@ -30,7 +30,8 @@ library LibString {
                 // The ASCII digit offset for '0' is 48.
                 let char := add(48, mod(n, 10))
 
-                // Write the current character into str.
+                // Write the current character into str. We could use mstore8 here, but we'd
+                // have to handle offsetting the pointer by 31 bytes which would take extra gas.
                 mstore(add(str, k), char)
 
                 k := sub(k, 1)
@@ -41,7 +42,7 @@ library LibString {
             str := add(str, k)
 
             // Set the length of the string to the correct value.
-            mstore(str, sub(78, k))
+            mstore(str, sub(96, k))
         }
     }
 }
